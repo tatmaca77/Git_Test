7cf5d6f0c3c73550f4359e88b2b8410b428efe9b
tatmaca77 <130807536+tatmaca77@users.noreply.github.com>
2023-04-15 03:31:54 +0200

    Update TestClass.java

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,7 +2,7 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.println("Git'den merhaba!");
-          System.out.println("GitHubd'dan merhaba!");
+          System.out.println("GitHub'dan merhaba!");
 
     }
 }
